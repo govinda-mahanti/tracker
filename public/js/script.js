9e20c7d17783de263a779c13d76d1d66bfbb1b93
govinda-mahanti <gmahanti955@gmail.com>
2025-06-11 00:01:55 +0530

    add

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,73 @@
 const socket = io();
 
+let map;
+let myMarker;
+const otherMarkers = {};
+
+// Initialize map only once
+function initMap(lat, lng) {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat, lng },
+    zoom: 16,
+  });
+
+  myMarker = new google.maps.Marker({
+    position: { lat, lng },
+    map: map,
+    title: "You",
+    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+  });
+}
+
+// Watch your own location
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
+
+      // Send to server
       socket.emit("send-location", { latitude, longitude });
+
+      // First-time map setup
+      if (!map) {
+        initMap(latitude, longitude);
+      }
+
+      // Update your marker and map center
+      myMarker.setPosition({ lat: latitude, lng: longitude });
+      map.setCenter({ lat: latitude, lng: longitude });
     },
-    (error) => {
-      console.log(error);
+    (err) => {
+      console.error("Geolocation error:", err);
     },
     {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge:0,
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
     }
   );
 }
 
-const map=L.map("map").setView([0,0], 10);
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "OpenStreetMap"
-}).addTo(map)
+// Receive other users' locations
+socket.on("receive-location", ({ id, latitude, longitude }) => {
+  const position = { lat: latitude, lng: longitude };
 
+  if (otherMarkers[id]) {
+    otherMarkers[id].setPosition(position);
+  } else {
+    otherMarkers[id] = new google.maps.Marker({
+      position,
+      map,
+      icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+      title: `User ${id}`
+    });
+  }
+});
 
-const markers ={};
-
-
-socket.on("receive-location", (data)=>{
-    const {id, latitude, longitude}=data;
-    map.setView([latitude, longitude]);
-    if (markers[id]){
-        markers[id].setLatLng([latitude, longitude]);
-    }
-    else{
-        markers[id]=L.marker([latitude, longitude]).addTo(map);
-    }
-
-})
-
-
-socket.on("user-disconnected", (id)=>{
-    if(markers[id]){
-        map.removeLayer(markers[id]);
-        delete markers[id];
-    }
-})
+// Remove marker if user disconnects
+socket.on("user-disconnected", (id) => {
+  if (otherMarkers[id]) {
+    otherMarkers[id].setMap(null);
+    delete otherMarkers[id];
+  }
+});
